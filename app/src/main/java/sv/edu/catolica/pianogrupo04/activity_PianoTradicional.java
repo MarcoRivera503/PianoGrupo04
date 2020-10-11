a9fe29ea889fb4e5c2ae7c4a52e9de66d3f86f99
sofiaescobar28 <josselyn.escobar@catolica.edu.sv>
2020-10-11 07:35:01 -0600

    no sirve el menu

--- app/src/main/java/sv/edu/catolica/pianogrupo04/activity_PianoTradicional.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo04/activity_PianoTradicional.java
@@ -1,10 +1,14 @@
 package sv.edu.catolica.pianogrupo04;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class activity_PianoTradicional extends AppCompatActivity {
@@ -71,5 +75,32 @@ public class activity_PianoTradicional extends AppCompatActivity {
         }else {
             sonido.start();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.piano_Tradicional:
+                break;
+            case R.id.piano_Infantil:
+                Intent objIntentInfantil = new Intent(activity_PianoTradicional.this, activity_Piano_Infantil_Selva.class);
+                startActivity(objIntentInfantil);
+            case R.id.piano_Instrumento:
+                Intent objIntentInstru = new Intent(activity_PianoTradicional.this, activity_Piano_Instrumental.class);
+                startActivity(objIntentInstru);
+            case R.id.acercaDe:
+                Intent objIntentAcerca = new Intent(activity_PianoTradicional.this, activity_AcercaDeNosotros.class);
+                startActivity(objIntentAcerca);
+            case R.id.salir:
+                finish();
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
