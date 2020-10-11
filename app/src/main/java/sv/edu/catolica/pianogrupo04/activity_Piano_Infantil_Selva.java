595dce6b39b0b1b1b4346c9e73288251c71faf6c
slManuel <jose.solis@catolica.edu.sv>
2020-10-11 10:59:36 -0600

    pianos terminados

--- app/src/main/java/sv/edu/catolica/pianogrupo04/activity_Piano_Infantil_Selva.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo04/activity_Piano_Infantil_Selva.java
@@ -2,13 +2,74 @@ package sv.edu.catolica.pianogrupo04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_Piano_Infantil_Selva extends AppCompatActivity {
-
+MediaPlayer sonido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__piano__infantil__selva);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    public void buho(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.buho);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void burro(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.burro);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void caballo(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.caballo);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void gallo(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.gallo);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void oveja(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.oveja);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void perro(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.perro);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void vaca(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.vaca);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+
 }
--- app/src/main/java/sv/edu/catolica/pianogrupo04/activity_Piano_Instrumental.java
+++ app/src/main/java/sv/edu/catolica/pianogrupo04/activity_Piano_Instrumental.java
@@ -2,13 +2,73 @@ package sv.edu.catolica.pianogrupo04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_Piano_Instrumental extends AppCompatActivity {
-
+    MediaPlayer sonido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__piano__instrumental);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+    public void arpa(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.arpa);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void bajo(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.bajo);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void bongos(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.bongos);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void flauta(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.flauta);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void piano(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.piano);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void tambor(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.tambor);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
+    }
+    public void trompeta(View v){
+        sonido = MediaPlayer.create(getApplicationContext(),R.raw.trompeta);
+        if (sonido.isPlaying()){
+            sonido.stop();
+        }else {
+            sonido.start();
+        }
     }
 }
